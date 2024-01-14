0e1ff51da4b32f53696bb6bff3914979903cdf82
Osada <osadasliit@gmail.com>
2024-01-14 16:36:09 +0530

    send and consume messages to and from activemq queue using rest

--- camel-tutorial/src/main/java/com/tutorial/routers/MessageRestServiceRouter.java
+++ camel-tutorial/src/main/java/com/tutorial/routers/MessageRestServiceRouter.java
@@ -6,6 +6,7 @@ import com.tutorial.model.Message;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import static org.apache.camel.model.rest.RestParamType.body;
@@ -49,6 +50,24 @@ public class MessageRestServiceRouter extends RouteBuilder {
                 .to("bean:messageService?method=updateMessage")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(204))
                 .setBody(constant(""));
+
+                //http://localhost:8002/api/activemq/public
+               // send messages to queue
+                rest().post("activemq/public")
+                        .produces(MediaType.APPLICATION_JSON_VALUE)
+                        .consumes(MediaType.APPLICATION_JSON_VALUE)
+                        .type(Message.class)
+                        .param()
+                        .name("body")
+                        .type(body)
+                        .description("The message to update")
+                        .endParam()
+                        .to("activemq:queue:publicQueue?exchangePattern=InOnly")
+                        .responseMessage().code(204).message("Message queue successfully updated").endResponseMessage();
+
+                      // consume messages from queue
+                       from("activemq:queue:publicQueue").log("Reading public message incoming - ${body}").end();
+
 
     }
 
